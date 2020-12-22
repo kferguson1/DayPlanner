39ba15bdd8000b3f229b980681fda6530802f7b1
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2020-12-21 20:48:20 -0500

    Added function to save to local storage

--- script.js
+++ script.js
@@ -9,4 +9,21 @@ $(document).ready(function() {
         nowHour24 = 13;
         nowHour12 = 1;
     }
+
+    let StoredPlans = JSON.parse(localStorage.getItem("Stored Plans"));
+    if (test) { console.log(StoredPlans); }
+
+    if (StoredPlans !==null) {
+        planTextArr = StoredPlans;
+    } else {
+        planTextArr = new Array(9);
+        planTextArr[4] = "Daily Meeting";
+    }
+
+    if (test) { console.log("full array of planned text", planTextArr); }
+    let $plannerDiv = $('#plannercContainer');
+    $plannerDiv.empty();
+
+    if (test) {console.log("current time", nowHour12); }
+    
 })
