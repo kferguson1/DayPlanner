c09607614b846c8bed08c7e2a6a1e26ffc02a6b3
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2020-12-21 23:35:13 -0500

    Updated Read Me

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # DayPlanner
+
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+You'll need to use a library like Moment.js to work with dates and times. Moment.js has historically been the most popular date/time library but is no longer supported by its developers. However, you can still use it for this project, or you can look into one of the following alternatives:
+Luxon
+Day.js
+date-fns
+js-Joda
+Whichever library you choose, be sure to read the documentation carefully!
+
+Steps
+- Added HTML, JS, and CSS Files
+- Added Moment Library
+- Added jQuery Library
+- Added rows for Time of day
+- Set  hour functions
+- Added CSS border shadow
+- Added function to save to local storage
+- Created form in html doc
+
+Screenshot
+![Screen Shot 2020-12-21 at 11 24 30 PM](https://user-images.githubusercontent.com/66282427/102849268-c912f780-43e4-11eb-915b-e709bf49a2fa.png)
--- script.js
+++ script.js
@@ -27,16 +27,31 @@ $(document).ready(function() {
     if (test) {console.log("current time", nowHour12); }
 
 })
-function updateRowColor ($hourRow,hour) {
-    if (test) { console.log("row color ", nowHour24, hour); }
-    if (hour < nowHour24) {
-        if (test) { console.log("lessThan"); }
-      $hourRow.css("background-color","lightgrey")
-    } else if ( hour > nowHour24) {
-      if (test) { console.log("greaterthan"); }
-      $hourRow.css("background-color","lightgreen")
+
+for (let hour = 9; hour <= 17; hour++) {
+    let index = hour - 9;
+    
+
+    let $rowDiv = $('<div>');
+    $rowDiv.addClass('row');
+    $rowDiv.addClass('plannerRow');
+    $rowDiv.attr('hour-index',hour);
+  
+
+    let $col2TimeDiv = $('<div>');
+    $col2TimeDiv.addClass('col-md-2');
+  
+
+    const $timeBoxSpn = $('<span>');
+    $timeBoxSpn.attr('class','timeBox');
+    
+    let displayHour = 0;
+    let ampm = "";
+    if (hour > 12) { 
+      displayHour = hour - 12;
+      ampm = "pm";
     } else {
-      if (test) { console.log("eqaul"); }
-      $hourRow.css("background-color","tomato")
+      displayHour = hour;
+      ampm = "am";
     }
-    }
+}
